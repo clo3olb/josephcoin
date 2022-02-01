134f9a31aee89d049db919bf7559d5fac367a59a
Hyeonwoo Kim <clo3olb@gmail.com>
2022-02-01 23:43:01 +0800

    created blockchain browser and add page

--- main.go
+++ main.go
@@ -9,22 +9,39 @@ import (
 	"github.com/clo3olb/josephcoin/blockchain"
 )
 
-const port string = ":4000"
+const (
+	port        string = ":4000"
+	templateDir string = "templates/"
+)
+
+var templates *template.Template
 
 type homeData struct {
 	PageTitle string
 	Blocks    []*blockchain.Block
 }
 
-func home(rw http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("templates/home.html"))
+func add(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		templates.ExecuteTemplate(rw, "add", nil)
+	case "POST":
+		r.ParseForm()
+		data := r.Form.Get("blockData")
+		blockchain.GetBlockchain().AddBlock(data)
+		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
+	}
+}
 
-	data := homeData{"home", blockchain.GetBlockchain().AllBlocks()}
-	tmpl.Execute(rw, data)
-	fmt.Fprint(rw, "Hello from home!")
+func home(rw http.ResponseWriter, r *http.Request) {
+	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
+	templates.ExecuteTemplate(rw, "home", data)
 }
 
 func main() {
+	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
+	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
+	http.HandleFunc("/add", add)
 	http.HandleFunc("/", home)
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
